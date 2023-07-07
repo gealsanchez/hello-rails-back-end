ad8cc16ca91bb8bf2a8bdff73e5bd7ee270218bb
gealsanchez <gealsanchez@gmail.com>
2023-07-06 19:58:55 -0500

    Backend setup

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,10 @@
+# class ApiController < ApplicationController
+class ApiController < ApplicationController
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
+end
